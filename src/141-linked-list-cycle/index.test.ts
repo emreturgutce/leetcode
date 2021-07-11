887a1f31b2ab4c0405bbf7098412b37b02e7cb8b
Emre Turgut <emreturgutcse@hotmail.com>
2021-07-11 13:48:14 +0300

    linked list cycle

--- src/141-linked-list-cycle/index.test.ts
+++ src/141-linked-list-cycle/index.test.ts
@@ -24,4 +24,19 @@ describe('Linked List Cycle', function () {
 
     expect(cycle).toEqual(false);
   });
+
+  it('Should detect the cycle when there are more than 3 nodes in the cycle', function () {
+    const node6 = new ListNode(6);
+    const node5 = new ListNode(5, node6);
+    const node4 = new ListNode(4, node5);
+    const node3 = new ListNode(3, node4);
+    const node2 = new ListNode(2, node3);
+    const node1 = new ListNode(1, node2);
+
+    node6.next = node2;
+
+    const cycle = hasCycle(node1);
+
+    expect(cycle).toEqual(true);
+  });
 });
